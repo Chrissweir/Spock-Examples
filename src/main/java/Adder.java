2e4e74b71abdfae5da9d9c11a1d907a0172e8d45
Christopher Weir <cweir@SIDERO.Athlone.Local>
2017-07-13 09:15:26 +0100

    Added the Adder and Multiply classes for testing

---
+++ src/main/java/Adder.java
@@ -0,0 +1,9 @@
+/**
+ * Created by cweir on 12/07/2017.
+ */
+public class Adder {
+
+    public int add(int a, int b){
+        return a + b;
+    }
+}
---
+++ src/main/java/Multiply.java
@@ -0,0 +1,8 @@
+/**
+ * Created by cweir on 12/07/2017.
+ */
+public class Multiply {
+    public int multiply(int a, int b){
+        return a * b;
+    }
+}
