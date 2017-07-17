5b10d5e0fbcfd94056da12bcd5ca4538ffc16cb4
Christopher Weir <cweir@SIDERO.Athlone.Local>
2017-07-17 13:49:54 +0100

    Added a parameterized example

--- ParameterizedTests/src/test/groovy/com/spock/examples/parameterized/ImageNameValidatorTest.groovy
+++ ParameterizedTests/src/test/groovy/com/spock/examples/parameterized/ImageNameValidatorTest.groovy
@@ -6,7 +6,8 @@ import spock.lang.Specification
  * Created by cweir on 17/07/2017.
  */
 class ImageNameValidatorTest extends Specification {
-    def "Valid images are JPG"(){
+    //This is an example of duplication of unit test i.e. bad practice
+   /* def "Valid images are JPG"(){
         given:"an image extension checker and a jpg file"
         ImageNameValidator validator = new ImageNameValidator()
         String pictureFile = "scenery.jpg"
@@ -31,5 +32,22 @@ class ImageNameValidatorTest extends Specification {
 
         expect: "that the filename is valid"
         !validator.isValidImageExtension(pictureFile)
+    }
+    */
+
+    def "Valid images are PNG and JPEG files"() {
+        given: "an image extension checker"
+        ImageNameValidator validator = new ImageNameValidator()
+
+        expect: "that only valid filenames are accepted"
+        validator.isValidImageExtension(pictureFile) == validPicture
+
+        where: "sample image names are"
+        pictureFile       || validPicture
+        "scenery.jpg"     || true
+        "house.jpeg"      || true
+        "car.png"         || true
+        "sky.tiff"        || false
+        "dance_bunny.gif" || false
     }
 }
