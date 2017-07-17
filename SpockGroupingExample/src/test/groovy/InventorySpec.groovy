db23bba8157d83a8f14125073c0734a903163bc8
Christopher Weir <cweir@SIDERO.Athlone.Local>
2017-07-17 12:59:28 +0100

    Added alternative grouping solution

--- SpockGroupingExample/src/test/groovy/InventorySpec.groovy
+++ SpockGroupingExample/src/test/groovy/InventorySpec.groovy
@@ -41,4 +41,45 @@ class InventorySpec extends Specification{
         warehouseInventory.availableOfProduct("panasonic") == 3
         warehouseInventory.availableOfProduct("jvc") == 2
     }
+    def "Buying products reduces the inventory availability (alt)"() {
+        given: "an inventory with products"
+        Product laptop = new Product(name: "toshiba", price: 1200, weight: 5)
+        Product camera = new Product(name: "panasonic", price: 350, weight: 2)
+        Product hifi = new Product(name: "jvc", price: 600, weight: 5)
+        WarehouseInventory warehouseInventory = new WarehouseInventory()
+        warehouseInventory.with {
+            preload laptop, 3
+            preload camera, 5
+            preload hifi, 2
+        }
+
+        and: "an empty basket"
+        EnterprisyBasket basket = new EnterprisyBasket()
+        basket.with {
+            setWarehouseInventory(warehouseInventory)
+            setCustomerResolver(new DefaultCustomerResolver())
+            setLanguage "english"
+            setNumberOfCaches 3
+            enableAutoRefresh()
+        }
+
+        when: "user gets a laptop and two cameras"
+        basket.with {
+            addProduct camera, 2
+            addProduct laptop
+        }
+
+        and: "user completes the transaction"
+        basket.checkout()
+
+        then: "warehouse is updated accordingly"
+        with(warehouseInventory)
+            {
+                !isEmpty()
+                warehouseInventory.getBoxesMovedToday() == 3
+                warehouseInventory.availableOfProduct("toshiba") == 2
+                warehouseInventory.availableOfProduct("panasonic") == 3
+                warehouseInventory.availableOfProduct("jvc") == 2
+            }
+    }
 }
