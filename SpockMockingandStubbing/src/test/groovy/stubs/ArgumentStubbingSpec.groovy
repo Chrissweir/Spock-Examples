d77614f1b7b77f2dccb13d5aa0c0d3305899dae1
Christopher Weir <cweir@SIDERO.Athlone.Local>
2017-07-20 11:12:53 +0100

    Added test case to stub all arguments

--- SpockMockingandStubbing/src/test/groovy/stubs/ArgumentStubbingSpec.groovy
+++ SpockMockingandStubbing/src/test/groovy/stubs/ArgumentStubbingSpec.groovy
@@ -48,6 +48,7 @@ class ArgumentStubbingSpec extends Specification{
         then: "order can be shipped right away"
         basket.canShipCompletely()
     }
+
     def "If warehouse has both products everything is fine"() {
         given: "a basket, a TV and a camera"
         Product tv = new Product(name:"bravia",price:1200,weight:18)
@@ -56,12 +57,32 @@ class ArgumentStubbingSpec extends Specification{
 
         and:"a warehouse with enough stock"
         WarehouseInventory inventory = Stub(WarehouseInventory)
+        //The "_" acts as 'I dont care what goes here'
         inventory.isProductAvailable( _, 1) >> true
         basket.setWarehouseInventory(inventory)
 
         when: "user checks out the tv and the camera"
         basket.addProduct tv
         basket.addProduct camera
+
+        then: "order can be shipped right away"
+        basket.canShipCompletely()
+    }
+    def "If warehouse is fully stocked everything is fine"() {
+        given: "a basket, a TV and a camera"
+        Product tv = new Product(name:"bravia",price:1200,weight:18)
+        Product camera = new Product(name:"panasonic",price:350,weight:2)
+        Basket basket = new Basket()
+
+        and:"a warehouse with limitless stock"
+        WarehouseInventory inventory = Stub(WarehouseInventory)
+        //The "_" acts as 'I dont care what goes here'
+        inventory.isProductAvailable( _, _) >> true
+        basket.setWarehouseInventory(inventory)
+
+        when: "user checks out the tv and the camera"
+        basket.addProduct tv,33
+        basket.addProduct camera,12
 
         then: "order can be shipped right away"
         basket.canShipCompletely()
