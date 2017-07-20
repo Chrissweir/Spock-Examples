6532c2e3e2123f61b26af34499687e0f956bb5be
Christopher Weir <cweir@SIDERO.Athlone.Local>
2017-07-20 15:50:12 +0100

    Created a test case to demonstrate the order of the interactions

--- SpockMockingandStubbing/src/test/groovy/mocks/OrderMockingSpec.groovy
+++ SpockMockingandStubbing/src/test/groovy/mocks/OrderMockingSpec.groovy
@@ -23,7 +23,27 @@ class OrderMockingSpec extends Specification{
 
         //Order of invocations in then block does not matter to Spock
         then: "credit card is charged and CC service is closed down"
-        1 * creditCardSevice.shutdown()
-        1 * creditCardSevice.sale(1200,customer)
+        1 * creditCardService.sale(1200,customer)
+        1 * creditCardService.shutdown()
+    }
+    def "Credit card connection is closed down in the end"(){
+        given: "a basket, a customer and a TV"
+        Product tv = new Product(name: "bravia", price: 1200, weight: 18)
+        BillableBasket basket = new BillableBasket()
+        Customer customer = new Customer(name: "John", vip: false, creditCard: "testCard")
+
+        and:"a credit card service"
+        CreditCardProcessor creditCardService = Mock(CreditCardProcessor)
+        basket.setCreditCardProcessor(creditCardService)
+
+        when:"a user checks out the tv"
+        basket.addProduct tv
+        basket.checkout()
+
+        then:"credit card is charged and"
+        1 * creditCardService.sale(_,_)
+
+        then:"the credit card service is closed down"
+        1 * creditCardService.shutdown()
     }
 }
