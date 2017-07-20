9ff7b160fe238f5a33f7902b5c576790a3a6667a
Christopher Weir <cweir@SIDERO.Athlone.Local>
2017-07-20 15:26:53 +0100

    Added another mocked method - verification

--- SpockMockingandStubbing/src/test/groovy/mocks/SimpleMockingSpec.groovy
+++ SpockMockingandStubbing/src/test/groovy/mocks/SimpleMockingSpec.groovy
@@ -2,6 +2,9 @@ package mocks
 
 import com.spock.example.Basket
 import com.spock.example.Product
+import com.spock.example.mocks.BillableBasket
+import com.spock.example.mocks.CreditCardProcessor
+import com.spock.example.mocks.Customer
 import com.spock.example.stubs.WarehouseInventory
 import spock.lang.Specification
 
@@ -21,5 +24,23 @@ class SimpleMockingSpec extends Specification{
 
         then:"order cannot be shipped"
         !basket.canShipCompletely()
+    }
+
+    def "Credit card connection is always closed down"(){
+        given:"a basket, a customer and a tv"
+        Product tv = new Product(name:"bravia",price: 1200,weight: 18)
+        BillableBasket basket = new BillableBasket()
+        Customer customer = new Customer(name: "John",vip: false,creditCard: "testCard")
+
+        and:"a credit card service"
+        CreditCardProcessor creditCardService = Mock(CreditCardProcessor)
+        basket.setCreditCardProcessor(creditCardService)
+
+        when:"a user checks out the tv"
+        basket.addProduct tv
+        basket.checkout(customer)
+
+        then:"connection is always closed at the end"
+        1 * creditCardService.shutdown()
     }
 }
