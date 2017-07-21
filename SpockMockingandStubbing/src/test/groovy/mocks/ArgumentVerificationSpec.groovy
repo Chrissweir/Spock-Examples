8311723ae25c66a5e5cc177c0ae20f131b4379d5
Christopher Weir <cweir@SIDERO.Athlone.Local>
2017-07-21 09:46:18 +0100

    Added test case to verify part of an object instance used as a mock argument

--- SpockMockingandStubbing/src/test/groovy/mocks/ArgumentVerificationSpec.groovy
+++ SpockMockingandStubbing/src/test/groovy/mocks/ArgumentVerificationSpec.groovy
@@ -29,4 +29,24 @@ class ArgumentVerificationSpec extends Specification{
         then: "credit card is charged"
         1 * creditCardService.sale(1550, customer)
     }
+
+    def "Vip status is correctly passed to credit card - vip"(){
+        given:"a basket, a customer and a tv"
+        Product tv = new Product(name: "bravia", price: 1200, weight: 18)
+        Product camera = new Product(name: "panasonic", price: 350, weight: 2)
+        BillableBasket basket = new BillableBasket()
+        Customer customer = new Customer(name: "John", vip: false, creditCard: "testCard")
+
+        and:"a cred card service"
+        CreditCardProcessor creditCardService = Mock(CreditCardProcessor)
+        basket.setCreditCardProcessor(creditCardService)
+
+        when:"a user checks out both products"
+        basket.addProduct(tv)
+        basket.addProduct(camera)
+        basket.checkout(customer)
+
+        then:"credit card is charged"
+        1 * creditCardService.sale(1550, {client -> client.vip == false})
+    }
 }
